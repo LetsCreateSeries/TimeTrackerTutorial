14eac8c88fc80e32c95210912790fa0b569cad31
Let's Create Series <letscreate.roblox@gmail.com>
2020-07-09 18:29:36 -0500

    Add FirebaseAnalytics to iOS project

--- TimeTrackerTutorial.iOS/AppDelegate.cs
+++ TimeTrackerTutorial.iOS/AppDelegate.cs
@@ -25,6 +25,8 @@ namespace TimeTrackerTutorial.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
+            Firebase.Core.App.Configure();
+
             return base.FinishedLaunching(app, options);
         }
     }

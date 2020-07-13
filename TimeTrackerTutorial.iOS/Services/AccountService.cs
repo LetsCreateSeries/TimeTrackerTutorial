49c015a39040b9400bf2b525060f3fb6de5d4403
Let's Create Series <letscreate.roblox@gmail.com>
2020-07-13 16:26:32 -0500

    Episode 13 - iOS Firebase Auth

---
+++ TimeTrackerTutorial.iOS/Services/AccountService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using TimeTrackerTutorial.iOS.Services;
+using TimeTrackerTutorial.Services.Account;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AccountService))]
+namespace TimeTrackerTutorial.iOS.Services
+{
+    public class AccountService : IAccountService
+    {
+        public AccountService()
+        {
+        }
+
+        public Task<double> GetCurrentPayRateAsync()
+        {
+            return Task.FromResult(10d);
+        }
+
+        public Task<bool> LoginAsync(string username, string password)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            Auth.DefaultInstance.SignInWithPasswordAsync(username, password)
+                .ContinueWith((task) => OnAuthCompleted(task, tcs));
+            return tcs.Task;
+        }
+
+        private void OnAuthCompleted(Task task, TaskCompletionSource<bool> tcs)
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                // something went wrong
+                tcs.SetResult(false);
+                return;
+            }
+            // user is logged in
+            tcs.SetResult(true);
+        }
+    }
+}

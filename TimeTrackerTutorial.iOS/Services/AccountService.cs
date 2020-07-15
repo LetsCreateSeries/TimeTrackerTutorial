5c870b9fda5c2c965da3d9e335bbefd988ab42fd
Let's Create Series <letscreate.roblox@gmail.com>
2020-07-14 21:49:21 -0500

    Episode 14: Firebase Phone Authentication on iOS

--- TimeTrackerTutorial.iOS/Services/AccountService.cs
+++ TimeTrackerTutorial.iOS/Services/AccountService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Firebase.Auth;
+using Foundation;
 using TimeTrackerTutorial.iOS.Services;
 using TimeTrackerTutorial.Services.Account;
 using Xamarin.Forms;
@@ -10,6 +11,9 @@ namespace TimeTrackerTutorial.iOS.Services
 {
     public class AccountService : IAccountService
     {
+        private TaskCompletionSource<bool> _phoneAuthTcs;
+        private string _verifactionId;
+
         public AccountService()
         {
         }
@@ -24,6 +28,42 @@ namespace TimeTrackerTutorial.iOS.Services
             var tcs = new TaskCompletionSource<bool>();
             Auth.DefaultInstance.SignInWithPasswordAsync(username, password)
                 .ContinueWith((task) => OnAuthCompleted(task, tcs));
+            return tcs.Task;
+        }
+
+        public Task<bool> SendOtpCodeAsync(string phoneNumber)
+        {
+            _phoneAuthTcs = new TaskCompletionSource<bool>();
+
+            PhoneAuthProvider.DefaultInstance.VerifyPhoneNumber(
+                phoneNumber,
+                null,
+                new VerificationResultHandler(OnVerificationResult));
+
+            return _phoneAuthTcs.Task;
+        }
+
+        private void OnVerificationResult(string verificationId, NSError error)
+        {
+            if (error != null)
+            {
+                // something went wrong
+                _phoneAuthTcs?.TrySetResult(false);
+                return;
+            }
+            _verifactionId = verificationId;
+            _phoneAuthTcs?.TrySetResult(true);
+        }
+
+        public Task<bool> VerifyOtpCodeAsync(string code)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            var credential = PhoneAuthProvider.DefaultInstance.GetCredential(
+                _verifactionId, code);
+            Auth.DefaultInstance.SignInWithCredentialAsync(credential)
+                .ContinueWith((task) => OnAuthCompleted(task, tcs));
+
             return tcs.Task;
         }
 

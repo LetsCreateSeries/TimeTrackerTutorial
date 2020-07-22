c57efe89f398c0b8178f245a7a8f9c77f215d3b8
Let's Create Series <letscreate.roblox@gmail.com>
2020-07-22 18:12:34 -0500

    Episode 17: iOS Cloud Firestore

--- TimeTrackerTutorial.iOS/Services/AccountService.cs
+++ TimeTrackerTutorial.iOS/Services/AccountService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Firebase.Auth;
 using Foundation;
 using TimeTrackerTutorial.iOS.Services;
+using TimeTrackerTutorial.Models;
 using TimeTrackerTutorial.Services.Account;
 using Xamarin.Forms;
 
@@ -77,6 +78,32 @@ namespace TimeTrackerTutorial.iOS.Services
             }
             // user is logged in
             tcs.SetResult(true);
+        }
+
+        public Task<AuthenticatedUser> GetUserAsync()
+        {
+            var tcs = new TaskCompletionSource<AuthenticatedUser>();
+
+            Firebase.CloudFirestore.Firestore.SharedInstance
+                .GetCollection("users")
+                .GetDocument(Auth.DefaultInstance.CurrentUser.Uid)
+                .GetDocument((snapshot, error) =>
+                {
+                    if (error != null)
+                    {
+                        // something went wrong
+                        tcs.TrySetResult(default(AuthenticatedUser));
+                        return;
+                    }
+                    tcs.TrySetResult(new AuthenticatedUser
+                    {
+                        Id = snapshot.Id,
+                        FirstName = snapshot.GetValue(new NSString("FirstName")).ToString(),
+                        LastName = snapshot.GetValue(new NSString("LastName")).ToString()
+                    });
+                });
+
+            return tcs.Task;
         }
     }
 }
